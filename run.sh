df5f00968b13155a22a695637c9d9a2ff8e6e53a
Joel Wurtz <jwurtz@jolicode.com>
2015-02-25 00:37:05 +0100

    Trying to fix travis tests

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 echo 1 > /tmp/build.status
 
 # run the build inside UML kernel
-./linux quiet mem=2G rootfstype=hostfs rw \
+sudo ./linux quiet mem=2G rootfstype=hostfs rw \
     eth0=slirp,,/usr/bin/slirp-fullbolt \
     init=$(pwd)/tests.sh WORKDIR=$(pwd) HOME=$HOME
 
